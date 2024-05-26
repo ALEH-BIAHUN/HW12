6919919035e968ea613bc0cef95ac806650fa589
Aleh Biahun <stones2875@gmail.com>
2024-05-26 11:39:51 +0200

    Solution of task number 12

---
+++ Task 1/src/Person.java
@@ -0,0 +1,58 @@
+public class Person {
+    private String name;
+    private String position;
+    private String placeOfWork;
+    private int age;
+    private int salary;
+
+    public Person() {
+    }
+
+    public Person(String position, String name, String placeOfWork, int age, int salary) {
+        this.position = position;
+        this.name = name;
+        this.placeOfWork = placeOfWork;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPlaceOfWork() {
+        return placeOfWork;
+    }
+
+    public void setPlaceOfWork(String placeOfWork) {
+        this.placeOfWork = placeOfWork;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
